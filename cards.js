fa9c0f4ed9a35446c09935e77b10c75c665fa414
GSnailG <61771862+GsnailG@users.noreply.github.com>
2021-09-15 08:46:18 +1200

    New Cards And Centering

--- cards.js
+++ cards.js
@@ -9,6 +9,14 @@ var prompts = [
     "Introducing the Mountainview Highschool ______________ club.",
     "The real reason why Mr Gibbs left",
     "A brand new“ position”, the _____________.",
+    "Nine out of Ten students agreed that there needs to be more _________ in the learning enviroment.",
+    "What left this stain on my couch?",
+    "Money can't buy me love, but it can buy me _________.",
+    "Bought a huge 70inch 4k tv, I can finaly watch ________ now.",
+    "I never leave the house without ______.",
+    "In order to be hip with the kids, (card reader) is actively engaged in ________.",
+    "Life would be better without ______.",
+    "Im not like other girls, I'm __________."
 ]
 
 function newPrompt() {
