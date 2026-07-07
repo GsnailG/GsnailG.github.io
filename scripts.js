b9fb9be4e353558d2cd683e9d353b04001177104
gareth <61771862+GsnailG@users.noreply.github.com>
2026-07-08 00:24:41 +1200

    Updated from Publii

--- scripts.js
+++
@@ -1,7 +0,0 @@
-function bg(){
-    var headerImgOptions = Array("url(/img/headers/1.jpg)", "url(/img/headers/2.jpg)");
-    var headerImgChoice = headerImgOptions[Math.floor(Math.random() * headerImgOptions.length)];
-    document.getElementById("topPic").style.backgroundImage = headerImgChoice;
-}
-
-bg()
